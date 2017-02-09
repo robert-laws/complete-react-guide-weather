9585612eeff3844db8c0e15ef025c97154f984c4
robert-laws <robaws@me.com>
2017-02-09 20:03:04 +0300

    about page - foundation

--- app/components/About.js
+++ app/components/About.js
@@ -3,8 +3,11 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h2>About</h2>
-      <p>This is about the weather app</p>
+      <h1 className="text-centered">About</h1>
+      <div className="callout primary">
+        <h5>This website is made using <a href="https://facebook.github.io/react">ReactJS</a></h5>
+        <h5>Weather is found using <a href="http://openweathermap.org">Open Weather Map</a></h5>
+      </div>
     </div>
   )
 }
